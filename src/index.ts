124cb2af9dbf9eacd6254e5e43c12b15b62c2742
OTKUSteyler <qsanom@proton.me>
2026-05-23 11:09:27 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,36 @@ import { findByProps } from "@vendetta/metro";
 import { instead } from "@vendetta/patcher";
 
 const MessageActions = findByProps("pinMessage", "unpinMessage");
+const ActionSheetUtils = findByProps("openLazy", "hideActionSheet");
 
 let patches: (() => void)[] = [];
 
 export default {
     name: "BypassPinPrompt",
     description: "Bypass the confirmation prompt when pinning or unpinning messages.",
-    authors: [{ name: "btmc727 (ported) (original) thororen" }],
+    authors: [{ name: "thororen (ported)" }],
 
     onLoad() {
-        if (!MessageActions) return;
+        if (!MessageActions || !ActionSheetUtils) return;
 
         patches.push(
-            instead("pinMessage", MessageActions, (args, orig) => {
-                return orig(...args);
-            })
-        );
+            instead("openLazy", ActionSheetUtils, (args, orig) => {
+                const sheet = args[1];
 
-        patches.push(
-            instead("unpinMessage", MessageActions, (args, orig) => {
+                if (sheet?.pin) {
+                    const { channelId, messageId } = sheet;
+                    MessageActions.pinMessage(channelId, messageId);
+                    ActionSheetUtils.hideActionSheet();
+                    return;
+                }
+
+                if (sheet?.unpin) {
+                    const { channelId, messageId } = sheet;
+                    MessageActions.unpinMessage(channelId, messageId);
+                    ActionSheetUtils.hideActionSheet();
+                    return;
+                }
+
                 return orig(...args);
             })
         );
